a8519db546c78eef53a86744517ae516a6639dca
MarkWilber <mwilberssl@gmail.com>
2019-08-08 20:16:17 -0700

    Added GloVeDict() to utility.py, and testGloVeDict() to testUtility.py.

--- utility/utility.py
+++ utility/utility.py
@@ -1,9 +1,18 @@
 import timeit
 import numpy as np
 import pandas as pd
+from pathlib import Path
+import pickle
 from sklearn.model_selection import train_test_split
 
 def timeUsage(func):
+    """
+    INPUT:
+        func	obj, function you want timed
+
+    This is a decorator that prints out the duration of execution for a
+    function. Formats differ for cases < 1 min, < 1 hour and >= 1 hour.
+    """
 
     def wrapper(*args, **kwargs):
         t0 = timeit.default_timer()
@@ -162,5 +171,56 @@ def splitBalanceDataFrameByClasses(df, classColumn, targetClassSize,
     return dfTrain, dfTest
 
 
-# @timeUsage
-# def 
+@timeUsage
+def GloVeDict(GloVeDir, embeddingSz=200):
+    """
+    INPUT:
+        GloVeDir	str, path to GloVe embedding data
+        embeddingSz	int, one of [50, 100, 200, 300], the dimension of
+                        the embedding vectors you want returned.
+
+    Returns a dict containing GloVe word embedding vectors of specified
+    dimensions, indexed by un-cased words, as computed by Stanford from the
+    Wikipedia 2014 data set. If a corresponding pickle file is found, it will
+    simply load and return the dict. Otherwise, it will extract the embeddings
+    from the source file, create the dict and save the pickled dict, before
+    returning the dict.
+    """
+
+    if embeddingSz not in [50, 100, 200, 300]:
+        raise ValueError(f"You supplied embeddingSz: {embeddingSz}, but it"
+                         " must be one of [50, 100, 200, 300].")
+
+    GloVeFile = Path(GloVeDir) / f"GloVe.6B.{embeddingSz:03d}.pkl"
+    if GloVeFile.exists():
+        print(f"Loading GloVe vectors from {GloVeFile} ...")
+        with open(GloVeFile, 'rb') as pickledGloVe:
+            return pickle.load(pickledGloVe)
+    else:    
+        RawGloVeFile = Path(GloVeDir) / f"glove.6B.{embeddingSz:03d}d.txt"
+        print(f"{GloVeFile} does not (yet) exist.")
+        print(RawGloVeFile)
+        if RawGloVeFile.exists():
+            print("Reading GloVe vectors from raw text file, "
+                  f"{RawGloVeFile} ...")
+            GloVeDict = {}
+            GloVeVects = RawGloVeFile.open(encoding='utf-8')
+            for l in GloVeVects:
+                parts = l.split()
+                word = parts[0]
+                coeffs = np.asarray(parts[1:], dtype='float32')
+                GloVeDict[word] = coeffs
+            GloVeVects.close()
+
+            print(f"vocab size: {len(GloVeDict.keys())}.")
+
+            print(f"Saving embeddings dict to {GloVeFile}.")
+            pFile = open(GloVeFile, 'wb')
+            pickle.dump(GloVeDict, pFile, -1)
+            pFile.close()
+
+            return GloVeDict
+        else:
+            raise FileNotFoundError("Can't extract GloVe vectors from non-"
+                                    f"existent raw text file, {RawGloVeFile}.")
+            return 1
